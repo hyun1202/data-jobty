ee5052a23c32df36448d8d809ccaa0d753e2a8ae
hyun <hyun1202a@gmail.com>
2024-05-04 22:26:28 +0900

    :sparkles: feat: db연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmConfigService } from "./config/typeorm.config.service";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+      inject: [ConfigService]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity({ name : "member" })
+export class User {
+  @PrimaryColumn()
+  member_id : string
+  @Column()
+  email : string
+  @Column()
+  pwd : string
+  @Column()
+  roles : string
+  @Column()
+  withdraw_dt : string
+  @Column()
+  status : number
+  @Column()
+  last_login_dt : string
+  @Column()
+  reg_dt : string
+  @Column()
+  update_dt : string
+}
