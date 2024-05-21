04ab81bf5d6167d9e1ff607c13a6e92add1c2b43
hyun <hyun1202a@gmail.com>
2024-05-21 17:21:16 +0900

    :sparkles: 인증 실패 시 에러 코드 반환 추가

--- src/common/exception/http.exception.filter.ts
+++ src/common/exception/http.exception.filter.ts
@@ -1,18 +1,22 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, UnauthorizedException } from "@nestjs/common";
 import { Response, Request } from "express";
 import { CommonResult } from "../response/common.result";
-import { TCommonCode } from "./error.code";
+import { ErrorCode, TCommonCode } from "./error.code";
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost): any {
     const status = exception.getStatus() || 200;
     const message = exception.message;
-    const errorCode: TCommonCode = exception?.errorCode;
+    let errorCode: TCommonCode = exception?.errorCode;
 
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
+
+    if (exception instanceof UnauthorizedException) {
+      errorCode = ErrorCode.OPERATION_NOT_AUTHORIZED;
+    }
 
     console.log(`status: ${status}`);
     console.log(`url: ${request.url}: ${request.method}()`);
